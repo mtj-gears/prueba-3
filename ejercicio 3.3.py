10926a33cc1ddd0f712a58c490d419844d57c767
mtj-gears <manuel.tlapa.juarez@gmail.com>
2022-06-13 15:32:15 -0500

    branch test actualizda

--- ejercicio 3.3.py
+++ ejercicio 3.3.py
@@ -2,14 +2,14 @@ print("introduce numero nuevo editado")
 num = int(input())
 if num < 1:
      False
-     print("false")
+     print("false rama")
 elif num == 2:
      True
      print("true")
 else:
     for i in range(2, num):
         if num % i == 0:
-             print("false2")
+             print("false2 rama")
              False
     print("true2")
     True
