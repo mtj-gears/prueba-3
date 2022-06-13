af4da9aa67e2f161fc7aacd0bb8ffe35e97d91c5
mtj-gears <manuel.tlapa.juarez@gmail.com>
2022-06-13 13:23:51 -0500

    Se agrega el Proyecto

---
+++ ejercicio 3.3.py
@@ -0,0 +1,15 @@
+print("introduce numero")
+num = int(input())
+if num < 1:
+     False
+     print("false")
+elif num == 2:
+     True
+     print("true")
+else:
+    for i in range(2, num):
+        if num % i == 0:
+             print("false2")
+             False
+    print("true2")
+    True
